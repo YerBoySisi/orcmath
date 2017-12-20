bfb22217ee9c494093e880dc9e2b458f4b4358e7
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-12-20 11:45:00 -0500

    Working on Bakery GUI bakebread

--- OrcMathGUI/src/guiSisi/Bakery.java
+++ OrcMathGUI/src/guiSisi/Bakery.java
@@ -10,129 +10,22 @@ import java.util.Scanner;
 public class Bakery {
 	
 	private ArrayList<Bread> breads;
-	private static Scanner in = new Scanner(System.in);
-	private static Bakery bakery = new Bakery();
 	
-	public static void main(String[] args) {
-		
-		String input;
-		
-		while(true) {
-			input = prompt();
-			interpretInput(input);
-		}
-		
-	}
-
 	public Bakery() {
 		
 		breads = new ArrayList<Bread>();
 		
 	}
 	
-	public static String prompt() {
+	public void bakeBread(String typ, String clr, int cal, int fat, int sdm, int crb) {
 		
-		System.out.println("Type 'bake' to bake a new bread, type 'save' to save your bakery.");
-		String input = in.nextLine();
-		
-		return input;
-		
-	}
-	
-	public static void interpretInput(String input) {
-		
-		if(input.equals("bake")) {
-			bakery.bakeBread();
-			bakery.displayBakery();
-		} else if(input.equals("save")) {
-			bakery.save();
-		} else {
-			System.out.println("Invalid input.");
-		}
-		
-	}
-	
-	public void bakeBread() {
-		
-		int count = 0;
-		
-		Bread brd = new Bread(null, null, 0, 0, 0, 0);
-		
-		while(count < 6) {
-			
-			if(count == 0) {
-				System.out.println("Type:");
-			} else if(count == 1) {
-				System.out.println("Color:");
-			} else if(count == 2) {
-				System.out.println("Calories:");
-			} else if(count == 3) {
-				System.out.println("Grams of Fat:");
-			} else if(count == 4) {
-				System.out.println("Grams of Sodium:");
-			} else if(count == 5) {
-				System.out.println("Grams of Carbohydrates:");
-			}
-			
-			String input= in.nextLine();
-			
-			if(count == 0) {
-				brd.setType(input);
-			} else if(count == 1) {
-				brd.setColor(input);
-			} else if(count == 2) {
-				
-				try {
-					brd.setCalories(Integer.parseInt(input));
-				} catch(NumberFormatException nfe) {
-					System.out.println("Invalid input. Enter a number");
-					input = in.nextLine();
-					count--;
-				}
-				
-			} else if(count == 3) {
-				
-				try {
-					brd.setFat(Integer.parseInt(input));
-				} catch(NumberFormatException nfe) {
-					System.out.println("Invalid input. Enter a number");
-					input = in.nextLine();
-					count--;
-				}
-				
-			} else if(count == 4) {
-				
-				try {
-					brd.setSodium(Integer.parseInt(input));
-				} catch(NumberFormatException nfe) {
-					System.out.println("Invalid input. Enter a number");
-					input = in.nextLine();
-					count--;
-				}
-				
-			} else if(count == 5) {
-				
-				try {
-					brd.setCarbs(Integer.parseInt(input));
-				} catch(NumberFormatException nfe) {
-					System.out.println("Invalid input. Enter a number");
-					input = in.nextLine();
-					count--;
-				}
-				
-			}
-			
-			count++;
-			
-		}
-		
-		breads.add(brd);
+		breads.add(new Bread(typ, clr, cal, fat, sdm, crb));
 		
 	}
 	
 	public void displayBakery() {
 		
-		System.out.println(bakery.getCSVContent());
+		System.out.println(getCSVContent());
 		
 	}
 	
--- OrcMathGUI/src/guiSisi/BakeryScreen.java
+++ OrcMathGUI/src/guiSisi/BakeryScreen.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import javax.swing.JFrame;
 
+import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 import guiTeacher.components.FileOpenButton;
+import guiTeacher.components.TextArea;
 import guiTeacher.components.TextField;
 import guiTeacher.interfaces.FileRequester;
 import guiTeacher.interfaces.Visible;
@@ -22,10 +24,12 @@ public class BakeryScreen extends FullFunctionScreen implements FileRequester {
 	private TextField fat;
 	private TextField sodium;
 	private TextField carbs;
+	private TextArea text;
 	private Button bake;
 	private Button save;
 	private FileOpenButton load;
 	private Button delete;
+	private Bakery bakery;
 	
 	public BakeryScreen(int width, int height) {
 		
@@ -35,16 +39,52 @@ public class BakeryScreen extends FullFunctionScreen implements FileRequester {
 
 	public void initAllObjects(List<Visible> viewObjects) {
 		
-		type = new TextField(20, 100, 200, 30, "" ,"Type");
-		color = new TextField(20, 150, 200, 30, "" ,"Color");
-		calories = new TextField(20, 200, 200, 30, "" ,"Calories");
-		fat = new TextField(20, 250, 200, 30, "" ,"Fat");
-		sodium = new TextField(20, 300, 200, 30, "" ,"Sodium");
-		carbs = new TextField(20, 350, 200, 30, "" ,"Carbohydrates");
+		bakery = new Bakery();
 		
-		bake = new Button(20, 400, 50, 30, "Bake", null);
-		save = new Button(70, 400, 50, 30, "Save", null);
-		delete = new Button(120, 400, 50, 30, "Delete", null);
+		type = new TextField(20, 100, 200, 30, "" ,"Type");
+		
+		color = new TextField(20, 150, 200, 30, "" ,"Color");
+		
+		calories = new TextField(20, 200, 200, 30, "" ,"Calories");
+		calories.setInputType(1);
+		
+		fat = new TextField(20, 250, 200, 30, "" ,"Fat");
+		fat.setInputType(1);
+		
+		sodium = new TextField(20, 300, 200, 30, "" ,"Sodium");
+		sodium.setInputType(1);
+		
+		carbs = new TextField(20, 350, 200, 30, "" ,"Carbohydrates");
+		carbs.setInputType(1);
+		
+		bake = new Button(20, 400, 50, 30, "Bake", new Action() {
+			
+			public void act() {
+				
+				bakeBread();
+				clearFields();
+				bakery.displayBakery();
+				
+			}
+			
+		});
+		
+		save = new Button(70, 400, 50, 30, "Save", new Action() {
+			
+			public void act() {
+				bakery.save();
+			}
+			
+		});
+		
+		delete = new Button(120, 400, 50, 30, "Delete", new Action() {
+			
+			public void act() {
+				
+			}
+			
+		});
+		
 		load = new FileOpenButton(170, 400, 50, 30, null, this);
 		
 		viewObjects.add(type);
@@ -62,9 +102,26 @@ public class BakeryScreen extends FullFunctionScreen implements FileRequester {
 	}
 
 	
-	public void n() {
+	public void bakeBread() {
+		
+		try {
+			bakery.bakeBread(type.getText(), color.getText(), Integer.parseInt(calories.getText()), Integer.parseInt(fat.getText()), 
+							 Integer.parseInt(sodium.getText()), Integer.parseInt(carbs.getText()));
+		} catch(NumberFormatException nfe) {
+			
+		}
 		
 		
+	}
+	
+	public void clearFields() {
+		
+		type.setText("");
+		color.setText("");
+		calories.setText("");
+		fat.setText("");
+		sodium.setText("");
+		carbs.setText("");
 		
 	}
 
